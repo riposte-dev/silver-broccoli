0d95eec3d165f0835163999a764469cce2b97cf7
riposte-dev <278319309+riposte-dev@users.noreply.github.com>
2026-08-08 17:28:49 +0900

    Insert markdown content into main section instead of body

--- parse-md-to-html.py
+++ parse-md-to-html.py
@@ -4,17 +4,19 @@ PATH_INPUT = "./input"
 PATH_OUTPUT = "./output"
 
 def create_html_file(md_file):
-    htmlTemplate = ""
     htmlFile = open(PATH_OUTPUT + "/" + md_file.replace(".md", ".html"), "w") # Create new or overwrite old file
+    
+    htmlTemplate = ""
 
     with open("./template.html") as template:
         htmlTemplate = template.read()
     
-    # Insert markdown content into the body of the html file
-    bodyIndex = htmlTemplate.find("</body>")
-    htmlContent = htmlTemplate[:bodyIndex] + markdownContent + "\n" + htmlTemplate[bodyIndex:]
-
+    # Insert markdown content into the main section of the HTML body
+    mainIndex = htmlTemplate.find("<main>")
+    htmlContent = htmlTemplate
+    
     htmlContent = htmlContent.replace("[title]", file.replace(".md", ""))
+    htmlContent = htmlContent.replace("[content]", markdownContent)
     
     htmlFile.write(htmlContent)
 
@@ -22,7 +24,8 @@ for file in os.listdir(PATH_INPUT):
     print("Parsing " + file + "...")
     
     markdownContent = ""
-    with open(PATH_INPUT + "/" + file) as content:
+
+    with open(PATH_INPUT + "/" + file, "r") as content:
         markdownContent = content.read()
 
     create_html_file(file)
