af2b8d2c363a3dbe8d847b6477e51244c8758d3a
riposte-dev <278319309+riposte-dev@users.noreply.github.com>
2026-08-08 17:30:11 +0900

    Remove old code for inserting markdown content into body

--- parse-md-to-html.py
+++ parse-md-to-html.py
@@ -6,14 +6,10 @@ PATH_OUTPUT = "./output"
 def create_html_file(md_file):
     htmlFile = open(PATH_OUTPUT + "/" + md_file.replace(".md", ".html"), "w") # Create new or overwrite old file
     
-    htmlTemplate = ""
+    htmlContent = ""
 
     with open("./template.html") as template:
-        htmlTemplate = template.read()
-    
-    # Insert markdown content into the main section of the HTML body
-    mainIndex = htmlTemplate.find("<main>")
-    htmlContent = htmlTemplate
+        htmlContent = template.read()
     
     htmlContent = htmlContent.replace("[title]", file.replace(".md", ""))
     htmlContent = htmlContent.replace("[content]", markdownContent)
