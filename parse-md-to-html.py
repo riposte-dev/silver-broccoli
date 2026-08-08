e5377094e7191b6b83ecd46268fd4bd801c6b1f2
riposte-dev <278319309+riposte-dev@users.noreply.github.com>
2026-08-08 16:25:34 +0900

    Define function that creates new HTML file

--- parse-md-to-html.py
+++ parse-md-to-html.py
@@ -5,3 +5,8 @@ with open(path) as content:
     markdownContent = content.read()
 
 print(markdownContent)
+
+def create_html_file():
+    htmlFile = open("./output/test.html", "x")
+
+create_html_file()
