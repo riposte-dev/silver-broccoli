915cfc5778dc5e6aea7f41e4a859a9fb09ce6159
riposte-dev <278319309+riposte-dev@users.noreply.github.com>
2026-08-08 17:42:17 +0900

    Display file creation & modified date

--- parse-md-to-html.py
+++ parse-md-to-html.py
@@ -1,7 +1,26 @@
 import os
+import time
 
 PATH_INPUT = "./input"
 PATH_OUTPUT = "./output"
+
+def get_file_creation_date(file):
+    created_seconds = os.path.getctime(PATH_INPUT + "/" + file)
+    created_formatted = time.ctime(created_seconds)
+
+    time_object = time.strptime(created_formatted)
+    date = time.strftime("%Y %m %d", time_object)
+
+    return date
+
+def get_file_modified_date(file):
+    modified_seconds = os.path.getmtime(PATH_INPUT + "/" + file)
+    modified_formatted = time.ctime(modified_seconds)
+    
+    time_object = time.strptime(modified_formatted)
+    date = time.strftime("%Y %m %d", time_object)
+
+    return date
 
 def create_html_file(md_file):
     htmlFile = open(PATH_OUTPUT + "/" + md_file.replace(".md", ".html"), "w") # Create new or overwrite old file
@@ -13,6 +32,8 @@ def create_html_file(md_file):
     
     htmlContent = htmlContent.replace("[title]", file.replace(".md", ""))
     htmlContent = htmlContent.replace("[content]", markdownContent)
+    htmlContent = htmlContent.replace("[created]", get_file_creation_date(md_file))
+    htmlContent = htmlContent.replace("[updated]", get_file_modified_date(md_file))
     
     htmlFile.write(htmlContent)
 
