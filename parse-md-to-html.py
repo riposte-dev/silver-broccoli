17cdd042caf58fc65ab046867e9ce32e9dc671e5
riposte-dev <278319309+riposte-dev@users.noreply.github.com>
2026-08-08 16:45:43 +0900

    Add comments to code

--- parse-md-to-html.py
+++ parse-md-to-html.py
@@ -6,13 +6,18 @@ with open(path) as content:
 
 def create_html_file():
     htmlTemplate = ""
-    htmlFile = open("./output/test.html", "w")
+    htmlFile = open("./output/test.html", "w") # Create new or overwrite old file
 
     with open("./template.html") as template:
         htmlTemplate = template.read()
     
+    # Insert markdown content into the body of the html file
     bodyIndex = htmlTemplate.find("<body>")
-    htmlContent = htmlTemplate[:bodyIndex + 6] + "\n" + markdownContent + htmlTemplate[bodyIndex + 6:]
+    htmlContent = htmlTemplate[:bodyIndex + 6]
+        + "\n"
+        + markdownContent
+        + htmlTemplate[bodyIndex + 6:]
+        # 6 is the string length of "<body>"
     
     htmlFile.write(htmlContent)
 
