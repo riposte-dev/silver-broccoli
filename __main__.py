a2ff57b8a3d153c55f242b8154ddd0d62789a483
riposte-dev <278319309+riposte-dev@users.noreply.github.com>
2026-08-15 00:44:11 +0900

    Create math_format module for formatting math block environments

--- __main__.py
+++ __main__.py
@@ -1,60 +1,12 @@
 import os
 import time
 import file_metadata
+import math_format
 
 PATH_INPUT = "./input/" # Directory for all source files in markdown
 PATH_OUTPUT = "./output/" # Directory for all generated html
 TEMPLATE_HTML = "./template.html"
 DATE_FORMAT = "%Y.%m.%d" # Default format for get_file_creation_date() and get_file_modified_date()
-
-def format_math_environment(lines):
-    """
-    Math block environments are defined like so:
-    $$
-    equation
-    $$
-
-    Parse to html like so:
-    <div class="math-block">
-    $$
-    equation
-    $$
-    </div>
-
-    MathJaX will automatically detect "$$" delimiters and render them
-
-    In-line delimiters "$" are rendered automatically as well so no need to check for them
-    """
-
-    # Create a list of indexes of all math block delimiters
-    math_delimiter_indexes = []
-
-    # Find all "$$" delimiters
-    for i in range(0, len(lines)):
-        if lines[i] == "$$":
-            math_delimiter_indexes.append(i)
-
-    # Since "$$" delimiters come in pairs, add "<div>" to the first and "</div" to the second
-    # Add opening <div> tag to first $$
-    for i in range(0, len(math_delimiter_indexes), 2):
-        math_delimiter_index = math_delimiter_indexes[i]
-        lines[math_delimiter_index] = "<div class='math-block'>\n" + lines[math_delimiter_index]
-    
-    # Add closing </div> tag to second $$
-    for i in range(0, len(math_delimiter_indexes), 2):
-        math_delimiter_index = math_delimiter_indexes[i+1] # Add to every other index
-        lines[math_delimiter_index] = lines[math_delimiter_index] + "\n</div>"
-    
-    # Finally, return a list of indexes of all math environment lines, including the delimiters
-    math_environment_indexes = []
-
-    # Since "$$" delimiters come in pairs, treat them as closed intervals where every index in between is also a math environment
-    for i in range(0, len(math_delimiter_indexes), 2):
-        for j in range(math_delimiter_indexes[i], math_delimiter_indexes[i+1] + 1): # From 'i' to 'i+1'
-            math_environment_indexes.append(j)
-    
-    return math_environment_indexes
-
 
 def format_code_environment(lines):
     """
@@ -177,9 +129,8 @@ def format_md_content(md_content):
 
     lines = md_content.split("\n") # Create a list of all lines in markdown content
 
-    # Remove any empty, new lines
     while "" in lines:
-        lines.remove("")
+        lines.remove("") # Remove all empty lines
 
     # Text environment
     # Create a list of indexes of all lines that are plain text
@@ -191,7 +142,7 @@ def format_md_content(md_content):
         text_environment_indexes.append(i)
     
     # Math block environment
-    math_environment_indexes = format_math_environment(lines)
+    math_environment_indexes = math_format.check_for_math_environment(lines)
 
     for i in math_environment_indexes:
         text_environment_indexes.remove(i)
@@ -208,7 +159,7 @@ def format_md_content(md_content):
     # Sum the content into html format
     for line in lines:
         html_body_content += line + "\n"
-    
+
     return html_body_content
 
 
---
+++ math_format.py
@@ -0,0 +1,66 @@
+"""
+Math block environments are written
+$$
+equation
+$$
+
+Parse to html like so:
+<div class="math-block">
+$$
+equation
+$$
+</div>
+
+MathJaX will automatically detect "$$" delimiters and render them
+
+In-line delimiters "$" are rendered automatically as well so no need to format them
+"""
+
+def check_for_math_delimiters(lines):
+    """
+    Given a list of lines from a Markdown file, return a list of indexes whose lines are "$$"
+    """
+
+    # Create a list of indexes for all math block delimiters
+    math_delimiter_indexes = [] 
+
+    # Find all "$$" delimiters
+    for i in range(0, len(lines)):
+        if lines[i] == "$$":
+            math_delimiter_indexes.append(i)
+
+    return math_delimiter_indexes
+
+
+def check_for_math_environment(lines):
+    """
+    Given a list of lines from a Markdown file, return a list of indexes whose lines are in a math environment
+    """
+
+    math_delimiter_indexes = check_for_math_delimiters(lines)
+    
+    # Create a list of indexes for all lines in a math environment, including the delimiters
+    math_environment_indexes = []
+
+    # Since "$$" delimiters come in pairs, treat them as closed intervals where every index in between is also a math environment
+    for i in range(0, len(math_delimiter_indexes), 2):
+        for j in range(math_delimiter_indexes[i], math_delimiter_indexes[i+1] + 1): # From 'i' to 'i+1'
+            math_environment_indexes.append(j)
+    
+    return math_environment_indexes
+
+
+def format_math_environment(lines):
+    math_delimiter_indexes = check_for_math_delimiters(lines)
+
+    # Since "$$" delimiters come in pairs, add "<div>" to the first and "</div>" to the second
+    # Add opening <div> tag to first $$
+    for i in range(0, len(math_delimiter_indexes), 2):
+        math_delimiter_index = math_delimiter_indexes[i]
+        lines[math_delimiter_index] = "<div class='math-block'>\n" + lines[math_delimiter_index]
+    
+    # Add closing </div> tag to second $$
+    for i in range(0, len(math_delimiter_indexes), 2):
+        math_delimiter_index = math_delimiter_indexes[i+1] # Add to every other index
+        lines[math_delimiter_index] = lines[math_delimiter_index] + "\n</div>"
+
